d7e8415c9226b7f4074b776f1a33a8f5ebd08559
Jose Arozena <josarozena@gmail.com>
2023-11-14 15:14:45 +0100

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState('');
+  const [word, setWord] = useState('');
+  const [replacement, setReplacement] = useState('');
+
+  const change = () => {
+    if(input.includes(word)) {
+      setInput(input.replaceAll(word, replacement));
+    }
+  }
+
+  return (
+    <>
+       <h1>Reemplazador de texto</h1>
+        <form>
+        <textarea 
+          id="input" 
+          name="text" 
+          rows="20" cols="60" 
+          placeholder="Introduce un texto..." 
+          value={input}
+          onChange={(e) => setInput(e.target.value)} 
+        />
+        <br/>
+        <input 
+          type="text" 
+          id="word" 
+          placeholder= "Introduce la palabra a reemplazar" 
+          value={word} 
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <input 
+          type="text" 
+          id="replacement" 
+          placeholder= "introduce la palabra de reemplazo" 
+          value={replacement}
+          onChange={(e) => setReplacement(e.target.value)}
+        />
+        <br/>
+        <input type="button" id="go" value="Reemplazar" onClick={() => change()}/>
+        </form>
+    </>
+  )
+}
+
+export default App
